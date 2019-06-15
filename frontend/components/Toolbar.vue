8d4bf8b24471947306888a61c5eb531e28b06f45
Dmitry K <kokontsev@gmail.com>
2019-06-15 21:13:03 +0300

    bugfix

--- frontend/components/Toolbar.vue
+++ frontend/components/Toolbar.vue
@@ -28,18 +28,40 @@
 
 <script>
 import Menu from "./Menu";
-import { mapGetters } from 'vuex';
+import { mapGetters } from "vuex";
 export default {
   components: { Menu },
-    computed: mapGetters({
-      containers: "containers/containers"
-    }),
-    data() {
-      return {
-        regions: [],
-        region: '',
-        companies: [],
-        company: {}
+  computed: mapGetters({
+    containers: "containers/containers"
+  }),
+  data() {
+    return {
+      regions: [],
+      region: "",
+      companies: [],
+      company: {}
+    };
+  },
+  async onRegionChange(e) {
+    try {
+      let { data } = await this.$axios.get("/api/companies", {
+        params: {
+          region: e
+        }
+      });
+      this.companies = data;
+      return data;
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  methods: {
+    async loadRegions() {
+      try {
+        let { data } = await this.$axios.get("/api/regions");
+        return data;
+      } catch (err) {
+        console.log(err);
       }
     },
     async onRegionChange(e) {
@@ -55,35 +77,12 @@ export default {
         console.log(err);
       }
     },
-    methods: {
-      async loadRegions() {
-        try {
-          let {data} = await this.$axios.get('/api/regions');
-          return data;
-        } catch (err) {
-          console.log(err);
-        }
-      },
-      async onRegionChange(e) {
-        try {
-          let {data} = await this.$axios.get('/api/companies', {
-            params: {
-              region: e
-            }
-          });
-          this.companies = data;
-          return data;
-        } catch (err) {
-          console.log(err);
-        }
-      },
-      async onCompanyChange(e) {
-        try {
-          let {data} = await this.$axios.get(`/api/companies/${e}/containers`);
-          this.$store.commit('containers/setContainers', data);
-        } catch (err) {
-          console.log(err);
-        }
+    async onCompanyChange(e) {
+      try {
+        let { data } = await this.$axios.get(`/api/companies/${e}/containers`);
+        this.$store.commit("containers/setContainers", data);
+      } catch (err) {
+        console.log(err);
       }
     }
   }
